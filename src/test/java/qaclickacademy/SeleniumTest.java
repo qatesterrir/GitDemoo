5fce7e45985bd6c94a0c59838f901f6601fbc46d
home <qatester.rir@gmail.com>
2020-08-22 22:50:02 -0400

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	//testng xml file from Maven
+	//how to execute all test cases from test folder with the help of command> mvn test
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");
+	}
+
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+}
